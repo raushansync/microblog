d9fb1b7e7e93d72bcdfeaf3784a0b5a52b41cfcc
Raushan Kumar <raushan@Raushans-MacBook-Air.local>
2026-08-10 21:59:39 +0530

    error handling and sys log implemented + email error feature

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from app import login
 from datetime import datetime, timezone
 from typing import Optional
@@ -15,6 +16,8 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique= True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,6 +27,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=robohash&s={size}'
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
